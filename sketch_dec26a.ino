5446b0d00a930985a570dbe4e088130c284ac328
Shubham Kumar <shubhamkumar8180323@gmail.com>
2019-02-08 09:30:31 +0530

    Add files via upload

---
+++ sketch_dec26a.ino
@@ -0,0 +1,19 @@
+#include <IRremote.h>  //including IRremote library file
+
+int ir = 12; //connecting data pin of reciever to pin 12 of arduino
+IRrecv recieve(ir);
+decode_results code;
+
+void setup() {
+  Serial.begin(9600);
+  recieve.enableIRIn();
+}
+
+void loop() {
+  if(recieve.decode(&code)){
+    Serial.println("HEX CODE RECORDED IS :");
+    Serial.println(code.value,HEX); //printing value of decoded result in HEX format
+    delay(50);
+    recieve.resume();  //ready to recieve next code
+  }
+}
